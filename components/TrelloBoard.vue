a1f9a01437424e535ff4dbfeb5990fa55d09bd8d
cgeb <ckg61386@gmail.com>
2023-03-31 14:40:06 +0900

    initial commit

---
+++ components/TrelloBoard.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="flex items-start overflow-x-auto gap-4">
+    <draggable
+      v-model="columns"
+      group="columns"
+      :animation="150"
+      handle=".drag-handle"
+      item-key="id"
+      class="flex gap-4 items-start"
+    >
+      <template #item="{ element: column }: { element: Column }">
+        <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
+          <header class="font-bold mb-4">
+            <DragHandle />
+            <input
+              class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+              @keyup.enter="($event.target as HTMLInputElement).blur()"
+              @keydown.backspace="column.title === '' ? (columns = columns.filter((c) => c.id !== column.id)) : null"
+              type="text"
+              v-model="column.title"
+            />
+          </header>
+          <draggable
+            v-model="column.tasks"
+            :group="{ name: 'tasks', pull: alt ? 'clone' : true }"
+            :animation="150"
+            handle=".drag-handle"
+            item-key="id"
+          >
+            <template #item="{ element: task }: { element: Task }">
+              <div>
+                <TrelloBoardTask :task="task" @delete="column.tasks = column.tasks.filter((t) => t.id !== $event)" />
+              </div>
+            </template>
+          </draggable>
+          <footer>
+            <NewTask @add="column.tasks.push($event)" />
+          </footer>
+        </div>
+      </template>
+    </draggable>
+    <button @click="createColumn" class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50">
+      + Add Another Column
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Column, Task } from "@/types";
+import { nanoid } from "nanoid";
+import draggable from "vuedraggable";
+
+const columns = useLocalStorage<Column[]>("trelloBoard", [
+  {
+    id: nanoid(),
+    title: "Backlog",
+    tasks: [
+      {
+        id: nanoid(),
+        title: "Create marketing landing page",
+        createdAt: new Date().toLocaleDateString(),
+      },
+      {
+        id: nanoid(),
+        title: "Develop cool new features",
+        createdAt: new Date().toLocaleDateString(),
+      },
+      {
+        id: nanoid(),
+        title: "Fix page nav bug",
+        createdAt: new Date().toLocaleDateString(),
+      },
+    ],
+  },
+  { id: nanoid(), title: "Selected for Dev", tasks: [] },
+  { id: nanoid(), title: "In Progress", tasks: [] },
+  { id: nanoid(), title: "QA", tasks: [] },
+  { id: nanoid(), title: "Complete", tasks: [] },
+]);
+
+const alt = useKeyModifier("Alt");
+
+function createColumn() {
+  columns.value.push({
+    id: nanoid(),
+    title: "",
+    tasks: [],
+  });
+
+  nextTick(() => {
+    const element = document.querySelector(".column:last-of-type .title-input") as HTMLInputElement;
+    element.focus();
+  });
+}
+</script>
+
+<style scoped></style>
